2fe6fa703770733548e8ce84c2d9ba4ce66b0836
Aetra <vava-hajek@hotmail.fr>
2020-02-06 09:27:14 +0100

    Ff

--- src/app/helpers/fake-backend.js
+++ src/app/helpers/fake-backend.js
@@ -1,5 +1,5 @@
 export function configureFakeBackend() {
-    let users = [{ id: 1, username: 'julien', password: 'gpAMQBgDCj7RT6xi', firstName: 'Admin', lastName: 'User' }];
+    let users = [{ id: 1, username: 'test', password: 'test', firstName: 'Admin', lastName: 'User' }];
     let realFetch = window.fetch;
     window.fetch = function (url, opts) {
         const isLoggedIn = opts.headers['Authorization'] === 'Bearer fake-jwt-token';
